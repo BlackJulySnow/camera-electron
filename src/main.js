868b2297ab5597be31e7454fa4b41cdb84ac3da7
B1GGersnow <2642448440@qq.com>
2023-08-31 23:59:54 +0800

    测试版本electron货查查

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import locale from "element-plus/lib/locale/lang/zh-cn";
+import locale from "element-plus/es/locale/lang/zh-cn";
 
 const debounce = (fn, delay) => {
     let timer = null;
