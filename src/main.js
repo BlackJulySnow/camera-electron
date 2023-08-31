62440f9756070121faa404aed8b6f5c8a9ff94de
B1GGersnow <2642448440@qq.com>
2023-08-31 14:37:58 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
+
+const app = createApp(App)
+app.config.globalProperties.axios = axios
+app.use(ElementPlus)
+app.mount('#app')
